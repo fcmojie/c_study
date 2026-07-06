2b621dc86c30b3bac4705894a227c40b331d0a59
fcmojie <1179672954@qq.com>
2026-07-06 14:56:18 +0800

    完成基本的服务器客户端通讯聊天功能

--- 01.network/client.c
+++ 01.network/client.c
@@ -12,8 +12,66 @@
 int main(int argc, char const *argv[]) {
 	int sock = 0;	//客户端socket文件描述符
 	struct sockaddr_in serv_addr;
-	char buffer[BUFFER_SIZE] = [0];
+	char buffer[BUFFER_SIZE] = {0};
 	fd_set read_fds;
 
+	// 创建socket
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket creation error");
+		return -1;
+	}
 
+	// 设置服务端地址
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		perror("inet_pton");
+		return -1;
+	}
+
+	// 连接服务器
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))  == -1) {
+		perror("connect faild");
+		return -1;
+	}
+	printf("[客户端] 连接服务器成功！可以开始聊天了(输入exit断开)\n");
+
+	//聊天循环
+	while (1)
+	{
+		FD_ZERO(&read_fds);
+		FD_SET(STDIN_FILENO, &read_fds);
+		FD_SET(sock, &read_fds);
+
+		if (select(sock + 1, &read_fds, NULL, NULL, NULL) == -1) {
+			perror("select");
+			break;
+		}
+
+		// 键盘输入
+		if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+			fgets(buffer, BUFFER_SIZE, stdin);
+			buffer[strcspn(buffer, "\n")] = '\0';
+			if (strcmp(buffer, "exit") == 0) {
+				printf("[客户端] 断开连接...\n");
+				send(sock, buffer, strlen(buffer), 0);
+				break;
+			}
+			send(sock, buffer, strlen(buffer), 0);
+		}
+
+		// 收到服务端消息
+		if (FD_ISSET(sock, &read_fds)) {
+			int valread = recv(sock, buffer, BUFFER_SIZE, 0);
+			if (valread <= 0) {
+				printf("[客户端] 服务器已断开连接\n");
+				break;
+			}
+			buffer[valread] = '\0';
+			printf("[服务器] %s\n", buffer);
+		}
+	}
+	close(sock);
+	return 0;
 }
