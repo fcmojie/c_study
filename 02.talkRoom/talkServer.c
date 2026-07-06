3c91b7b34b882f59f0a99e8ea23be290481adf2c
fcmojie <1179672954@qq.com>
2026-07-06 16:57:55 +0800

    talkroom

---
+++ 02.talkRoom/talkServer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]) {
+    // printf("argc:%d", argc);
+    // printf("argv: %s", argv[1]);
+
+    char *portstr = "";
+    if (argc >= 2) {
+        portstr = argv[1];
+    }
+    int port = 
+    
+    return 0;
+}
